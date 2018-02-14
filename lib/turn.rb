b554442dc7d2d8534d3df82d3c8930ff61318d4f
David Kischkower <david.kischkower@wework.com>
2018-02-14 15:08:30 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,11 +3,11 @@ def convert_to_index(user_input)
 end
 
 def display_board(array)
-  puts " #{array[0]}  | #{array[1]}  | #{array[2]} "
+  puts " #{array[0]} | #{array[1]} | #{array[2]} "
   puts "-----------"
-  puts " #{array[3]}  | #{array[4]}  | #{array[5]} "
+  puts " #{array[3]} | #{array[4]} | #{array[5]} "
   puts "-----------"
-  puts " #{array[6]}  | #{array[7]}  | #{array[8]} "
+  puts " #{array[6]} | #{array[7]} | #{array[8]} "
 
 end 
 
